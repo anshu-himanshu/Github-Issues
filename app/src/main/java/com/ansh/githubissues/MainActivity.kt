da1bf79050a202f387b9d5cf0780333054a6f69c
Himanshu <himanshux688@gmail.com>
2022-10-28 19:03:32 +0530

    basic frontend Implemented

--- app/src/main/java/com/ansh/githubissues/MainActivity.kt
+++ app/src/main/java/com/ansh/githubissues/MainActivity.kt
@@ -3,19 +3,22 @@ package com.ansh.githubissues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.ansh.githubissues.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val issuesApi = RetrofitHelper.getInstance().create(GithubApi::class.java)
         GlobalScope.launch {
             val result = issuesApi.getIssues("closed",2)
             if (result!=null){
-
 
                 val issuesList =  result.body()
                 issuesList?.forEach {
---
+++ app/src/main/java/com/ansh/githubissues/models/IssuesModel.kt
@@ -0,0 +1,9 @@
+package com.ansh.githubissues.models
+
+data class IssuesModel(
+    val title: String = "",
+    val createdDate: String = "",
+    val closedDate: String = "",
+    val userName: String = "",
+    val userImageUrl: String = ""
+)
